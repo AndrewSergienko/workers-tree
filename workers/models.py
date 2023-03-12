538a4d4b4dcc070724ab462e2febe5faf4d648d0
Andrew Sergienko <sergienkoandrew9922@gmail.com>
2023-03-12 11:41:23 +0200

    created Worker model

--- workers/models.py
+++ workers/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from mptt.models import MPTTModel, TreeForeignKey
 
-# Create your models here.
+POSITION_CHOISES = (
+    ("1", "Директор"),
+    ("2", "Зам. директора"),
+    ("3", "Начальник відділу"),
+    ("4", "Зам. начальника відділу"),
+    ("5", "Старший менеджер"),
+    ("6", "Менеджер"),
+    ("7", "Офісний працівник"),
+)
+
+
+class Worker(MPTTModel):
+    parent = TreeForeignKey(
+        "self",
+        null=True,
+        blank=True,
+        related_name="children",
+        on_delete=models.SET_NULL,
+    )
+    full_name = models.CharField(max_length=150)
+    position = models.CharField(choices=POSITION_CHOISES, max_length=1)
+    date_of_employment = models.DateField(auto_now_add=True)
+    email = models.EmailField()
